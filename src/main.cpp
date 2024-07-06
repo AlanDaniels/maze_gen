8bd3edbdea9bc1651cbe0540c32768f81d293733
Alan Daniels <alan@alandaniels.com>
2024-07-05 21:00:31 -0400

    Cleaner data structures

--- src/main.cpp
+++ src/main.cpp
@@ -24,61 +24,69 @@ constexpr unsigned int NUM_CHANNELS = 3;
 constexpr unsigned int INITIAL_VALUE = 0;
 
 
-class Coord
+class ScreenCoord
 {
 public:
-    Coord() = delete;
-    Coord(int x, int y) : m_x(x), m_y(y) {}
-    Coord(const Coord &that): m_x(that.m_x), m_y(that.m_y) {}
-    int m_x;
-    int m_y;
+    ScreenCoord() = delete;
+    ScreenCoord(int px, int py) : x(px), y(py) {}
+    ScreenCoord(const ScreenCoord &that): x(that.x), y(that.y) {}
+    int x;
+    int y;
 };
 
 
-class Connection
+class MazeCell
 {
 public:
-    Connection() = delete;
+    MazeCell() = delete;
+    MazeCell(int pw, int ph) : w(pw), h(ph) {}
+    MazeCell(const MazeCell &that): w(that.w), h(that.h) {}
+    int w;
+    int h;
+};
 
-    Connection(int begin_w, int begin_h, int end_w, int end_h) :
-        m_begin_w(begin_w), m_begin_h(begin_h), m_end_w(end_w), m_end_h(end_h)
+
+class MazeConnection
+{
+public:
+    MazeConnection() = delete;
+
+    MazeConnection(const MazeCell& pbegin, const MazeCell &pend) :
+        begin(pbegin), end(pend)
     {
         // Bounds check.
-        assert((m_begin_w >= 0) && (m_begin_w < GRID_WIDTH));
-        assert((m_begin_h >= 0) && (m_begin_h < GRID_HEIGHT));
-        assert((m_end_w >= 0) && (m_end_w < GRID_WIDTH));
-        assert((m_end_h >= 0) && (m_end_h < GRID_HEIGHT));
+        assert((begin.w >= 0) && (begin.w < GRID_WIDTH));
+        assert((begin.h >= 0) && (begin.h < GRID_HEIGHT));
+        assert((end.w >= 0) && (end.w < GRID_WIDTH));
+        assert((end.h >= 0) && (end.h < GRID_HEIGHT));
 
         // Connections should only span two cells.
-        assert((abs(m_begin_w - m_end_w) + abs(m_begin_h - m_end_h)) == 1);
+        assert((abs(begin.w - end.w) + abs(begin.h - end.h)) == 1);
     }
 
-    Connection(const Connection &that) :
-        m_begin_w(that.m_begin_w), m_begin_h(that.m_begin_h), 
-        m_end_w(that.m_end_w), m_end_h(that.m_end_h)
+    MazeConnection(const MazeConnection &that) :
+        begin(that.begin), end(that.end)
     {}
 
-    int m_begin_w;
-    int m_begin_h;
-    int m_end_w;
-    int m_end_h;
+    MazeCell begin;
+    MazeCell end;
 };
 
 
 // Calculate where to draw the maze sell.
-Coord calc_offset(int w, int h)
+ScreenCoord calc_offset(int w, int h)
 {
     int x = ((CELL_SIZE_PIX + (CELL_BORDER_PIX - 1)) * w) + OUTER_BORDER_PIX;
     int y = ((CELL_SIZE_PIX + (CELL_BORDER_PIX - 1)) * h) + OUTER_BORDER_PIX;
-    return Coord(x, y);
+    return ScreenCoord(x, y);
 }
 
 
 // Testing out connections.
-std::vector<Connection> connections = {
-    {5, 2, 5, 3},
-    {5, 2, 4, 2},
-    {5, 3, 5, 4}
+std::vector<MazeConnection> connections = {
+    {{5, 2}, {5, 3}},
+    {{5, 2}, {4, 2}},
+    {{5, 3}, {5, 4}}
 };
 
 
@@ -89,24 +97,23 @@ int main()
     cimg_library::CImgDisplay display(image, "Maze Generating");
 
     // Draw the maze cells.
-    // Draw the maze cells.
     for (int w = 0; w < GRID_WIDTH; w++) {
         for (int h = 0; h < GRID_HEIGHT; h++) {
-            Coord coord = calc_offset(w, h);
+            ScreenCoord coord = calc_offset(w, h);
             uint8_t gray[] = { 0x80, 0x80, 0x80 };
-            image.draw_rectangle(coord.m_x, coord.m_y, coord.m_x + CELL_SIZE_PIX, coord.m_y + CELL_SIZE_PIX, gray);
+            image.draw_rectangle(coord.x, coord.y, coord.x + CELL_SIZE_PIX, coord.y + CELL_SIZE_PIX, gray);
         }
     }
 
     // Draw the connections.
     for (auto conn : connections) {
-        Coord begin_coord = calc_offset(conn.m_begin_w, conn.m_begin_h);
-        Coord end_coord = calc_offset(conn.m_end_w, conn.m_end_h);
+        ScreenCoord begin_coord = calc_offset(conn.begin.w, conn.begin.h);
+        ScreenCoord end_coord = calc_offset(conn.end.w, conn.end.h);
 
-        int min_x = std::min(begin_coord.m_x, end_coord.m_x) + SQUISH_PIX;
-        int max_x = std::max(begin_coord.m_x, end_coord.m_x) + CELL_SIZE_PIX - SQUISH_PIX;
-        int min_y = std::min(begin_coord.m_y, end_coord.m_y) + SQUISH_PIX;
-        int max_y = std::max(begin_coord.m_y, end_coord.m_y) + CELL_SIZE_PIX - SQUISH_PIX;
+        int min_x = std::min(begin_coord.x, end_coord.x) + SQUISH_PIX;
+        int max_x = std::max(begin_coord.x, end_coord.x) + CELL_SIZE_PIX - SQUISH_PIX;
+        int min_y = std::min(begin_coord.y, end_coord.y) + SQUISH_PIX;
+        int max_y = std::max(begin_coord.y, end_coord.y) + CELL_SIZE_PIX - SQUISH_PIX;
 
         printf("%d, %d, %d, %d\n", min_x, max_x, min_y, max_y);
         uint8_t white[] = { 0xFF, 0xFF, 0xFF };
@@ -115,13 +122,13 @@ int main()
 
     // Draw the connections.
     for (auto conn : connections) {
-        Coord begin_coord = calc_offset(conn.m_begin_w, conn.m_begin_h);
-        Coord end_coord = calc_offset(conn.m_end_w, conn.m_end_h);
+        ScreenCoord begin_coord = calc_offset(conn.begin.w, conn.begin.h);
+        ScreenCoord end_coord = calc_offset(conn.end.w, conn.end.h);
 
-        int min_x = std::min(begin_coord.m_x, end_coord.m_x) + SQUISH_PIX;
-        int max_x = std::max(begin_coord.m_x, end_coord.m_x) + CELL_SIZE_PIX - SQUISH_PIX;
-        int min_y = std::min(begin_coord.m_y, end_coord.m_y) + SQUISH_PIX;
-        int max_y = std::max(begin_coord.m_y, end_coord.m_y) + CELL_SIZE_PIX - SQUISH_PIX;
+        int min_x = std::min(begin_coord.x, end_coord.x) + SQUISH_PIX;
+        int max_x = std::max(begin_coord.x, end_coord.x) + CELL_SIZE_PIX - SQUISH_PIX;
+        int min_y = std::min(begin_coord.y, end_coord.y) + SQUISH_PIX;
+        int max_y = std::max(begin_coord.y, end_coord.y) + CELL_SIZE_PIX - SQUISH_PIX;
 
         printf("%d, %d, %d, %d\n", min_x, max_x, min_y, max_y);
         uint8_t white[] = { 0xFF, 0xFF, 0xFF };
